9246a7ee0f0f4cc6c1fc7a48d9a2c6bdbef0f26c
DOrcas3 <kehindedorcas999@gmail.com>
2023-12-20 17:40:10 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,27 +1,16 @@
 #include "main.h"
+
 /**
- * _isalpha - return 1 when c is cap or low
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
  *
- * Description: we are using _putchar
- *
- * @c: collect the character
- *
- * Return: 1 if c is low or cap otherwise 0
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	char lower, upper;
-	int letter = 0;
-
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-		for (upper = 'A'; upper <= 'z'; upper++)
-		{
-			if (lower == c || upper == 'C')
-			{
-				letter = 1;
-			}
-		}
-	}
-	return (letter);
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
